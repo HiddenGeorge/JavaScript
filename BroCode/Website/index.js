6849901c1da3ad6a5ac02efbdb66e1328b0bf3d8
HiddenGeorge <georgeartifon@gmail.com>
2024-04-26 09:32:30 -0300

    Bro Code, first steps

---
+++ BroCode/Website/index.js
@@ -0,0 +1,26 @@
+console.log("Hello");
+console.log("I like pizza");
+/*                                      Alert for the site, I comment this because this don't stop, they are annoying
+window.alert("This is an alert!");
+window.alert("I like pizza");
+*/
+
+// document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myP").textContent = "Oh minha Godi";
+
+//This is a comment
+
+/*
+This
+is
+a
+comment
+*/
+
+let x;
+x = 123;
+
+let y = 344;
+
+console.log(x);
+console.log(y);
